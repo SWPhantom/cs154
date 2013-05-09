d17e1cc81598af6f886478240b47d3b20b367b6c
Zhenya Frolov <hellcomestotown@hotmail.com>
2013-05-08 18:16:00 -0700

    Changed all files to bare minimum skeleton code.
    Everything is clean and ready for actual work. According to our
    instructions, we will need a main file for testing, but we're not
    submitting it. We really need to only worry about the functions
    in cachesim.c.

---
+++ Project3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "cachesim.c"
+
+//CS154 Project 3
+//Brian Ly, Yevgen Frolov, Justin Flores
+
+
+int main(int argc, char *argv[])
+{
+	
+}
+
