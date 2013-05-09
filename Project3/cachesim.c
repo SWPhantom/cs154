1ca955a192c0cb5edffcea5f19a9d341c3ba8ac5
Zhenya Frolov <hellcomestotown@hotmail.com>
2013-05-08 19:52:28 -0700

    Created rudimentary structure. Linker issues.
    It will not compile due to function/variable declarations. The
    code may need a cachesim.h file to declare functions and be
    included in the main file. Asking on the class forum.

--- Project3/cachesim.c
+++ Project3/cachesim.c
@@ -4,8 +4,29 @@
 
 //CS154 Project 3
 //Brian Ly, Yevgen Frolov, Justin Flores
+//Project Source: http://www.cs.ucsb.edu/~franklin/154/projects/Project3/
 
+////=========================Declarations==============================================
+typedef struct _cache{
+	int cacheSize;
+	int blockSize;
+	int type;
+	
+	//POSSIBLY NEED ARRAY STRUCTURE OF INTS THAT CORRESPOND TO THE
+	//ACTUAL CACHE TABLE. THIS COULD BE A POINTER THAT IS MALLOCed
+	//WHEN THE CACHE IS CREATED. THE SIZE OF THE CACHE, THE BLOCK
+	//SIZE AND THE TYPE WILL INFLUENCE HOW BIG THE SAID TABLE WILL
+	//BE
+	
+	int misses;
+	int accesses;
+	int totalAccessTime;
+} Cache;
+
+////=========================Declarations End==========================================
+//-------------------------------------------------------------------------------------
 ////=========================Function Headers==========================================
+
 /*
  *You have a struct that contains all of the information for one cache. In this
  *function, you create the cache and initialize it, returning a pointer to the struct.
@@ -35,25 +56,50 @@ int accessesSoFar(void *cache);
  *This returns the total number of cycles that all of the accesses have taken so far.
  */
 int totalAccessTime(void *cache);
-////=========================Function Headers End======================================
 
+////=========================Function Headers End======================================
+//-------------------------------------------------------------------------------------
 ////=========================Function Implementations==================================
-void *createAndInitialize(int blocksize, int cachesize, int type); 
+
+void *createAndInitialize(int blocksize, int cachesize, int type){
+	Cache newCache;
+	newCache.blockSize = blocksize;
+	newCache.cacheSize = cachesize;
+	newCache.type = type;
+	Cache *outputPointer = &newCache;
+}
 
 
 int accessCache(void *cache, int address){
-
+	Cache *inCache = cache;
+	//Direct-mapped cache
+	if(inCache->type == 0){
+		
+	}else
+	//Pseudo-associative cache
+	if(inCache->type == 1){
+		
+	}else
+	//4-way set associative cache
+	if(inCache->type == 2){
+		
+	}else{//Something went wrong and the type was not set correctly.
+		return -1;
+	}
 }
 
-int missesSoFar(void *cache);{
-
+int missesSoFar(void *cache){
+	Cache *inCache = cache;
+	return inCache->misses;
 }
 
-int accessesSoFar(void *cache);{
-
+int accessesSoFar(void *cache){
+	Cache *inCache = cache;
+	return inCache->accesses;
 }
 
 int totalAccessTime(void *cache){
-
+	Cache *inCache = cache;
+	return inCache->totalAccessTime;
 }
 ////=========================Function Implementations End==============================
--- Project3/main.c
+++ Project3/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#include "cachesim.c"
+#include "cachesim.c"////CURRENTLY PROBLEMATIC
 
 //CS154 Project 3
 //Brian Ly, Yevgen Frolov, Justin Flores
-
 
 int main(int argc, char *argv[])
 {
