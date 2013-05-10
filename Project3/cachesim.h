a5a5c9269cac961a3a92fe35d45a8027ba418f97
Zhenya Frolov <hellcomestotown@hotmail.com>
2013-05-09 17:04:49 -0700

    ACTUALLY added the .h file
    Forgot to manually add it before.

---
+++ Project3/cachesim.h
@@ -0,0 +1,33 @@
+#ifndef CACHESIM_H
+#define CACHESIM_H
+/*
+ *You have a struct that contains all of the information for one cache. In this
+ *function, you create the cache and initialize it, returning a pointer to the struct.
+ *Because you are determining the struct, you return a void * to our main. Type 0 is a
+ *direct-mapped cache. Type 1 is a pseudo-associative cache. Type 2 is a 4-way set
+ *associative cache.
+ */
+void *createAndInitialize(int blocksize, int cachesize, int type);
+
+/*
+ *In this function, we access the cache with a particular address. If the address
+ *results in a hit, return 1. If it is a miss, return 0.
+ */
+int accessCache(void *cache, int address);
+
+/*
+ *This returns the number of misses that have occurred so far
+ */
+int missesSoFar(void *cache);
+
+/*
+ *This returns the number of accesses that have occurred so far
+ */
+int accessesSoFar(void *cache);
+
+/*
+ *This returns the total number of cycles that all of the accesses have taken so far.
+ */
+int totalAccessTime(void *cache);
+
+#endif
