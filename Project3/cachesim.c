f1b0a125801e3b1d323c03876693ea0ae76cbb40
Yevgen Frolov <frolov@mcl-15.cs.ucsb.edu>
2013-05-16 17:57:50 -0700

    Changed accesses incrementor. Does extra access in Pseudoassociative cache.

--- Project3/cachesim.c
+++ Project3/cachesim.c
@@ -97,6 +97,7 @@ int accessCache(void *cache, int address){
 			}
 			//Miss
 			else{
+				++(inCache->accesses);
 				if(inCache->cacheBlock[offset+1] == (address>>calcLog(inCache->blockSize))){
 					inCache->cacheBlock[offset+1] = inCache->cacheBlock[offset];
 					inCache->cacheBlock[offset] = (address>>calcLog(inCache->blockSize));
